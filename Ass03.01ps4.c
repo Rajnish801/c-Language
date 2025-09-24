d746e90f679c59aadad83309f910ae8824be782d
Rajnish Kumar Singh <rajnishkrsingh801@gmail.com>
2025-09-24 14:58:47 +0530

    Add files via upload

---
+++ Ass03.01ps4.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main() {
+  int a,b,c;
+    printf("enter the number a:");
+    scanf("%d",&a);
+    printf("enter the number b:");
+    scanf("%d",&b);
+    printf("enter the number c:");
+    scanf("%d",&c);
+    if (a>=b){
+        if(a>=c){
+            printf("The largest number is:- %d\n",a);
+        }
+        else{
+            printf("The largest number is:-%d\n",c);
+        }
+    }
+    else{
+        if(b>=c){
+            printf("The largest number is:-%d\n",b);
+        }
+        else{
+             printf("The largest number id :-%d\n",c);
+        }
+    }
+
+  return 0;
+}
