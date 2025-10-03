167131c7ee744377e81caa437c552109762b0028
Rajnish Kumar Singh <rajnishkrsingh801@gmail.com>
2025-10-02 23:57:22 -0500

    cal volume1.c

--- cal volume1.c
+++ cal volume1.c
@@ -1,14 +1,31 @@
 #include <stdio.h>
 
 int main() {
-    float centigrade, fahrenheit;
+    double radius;
+    double volume;
 
-    printf("Enter temperature in Centigrade: ");
-    scanf("%f", &centigrade);
+    printf("--- Sphere Volume Calculator ---\n");
+    printf("This program calculates the volume of a sphere.\n\n");
 
-    fahrenheit = (9 * centigrade) / 5 + 32;
+    printf("Enter the radius of the sphere (in units): ");
 
-    printf("Temperature in Fahrenheit: %.6f\n", fahrenheit);
+   
+    if (scanf("%lf", &radius) != 1) {
+        printf("Error: Invalid input. Please enter a number.\n");
+        return 1; 
+    }
 
-    return 0;
+    if (radius < 0) {
+        printf("Error: Radius cannot be negative. Please enter a positive value.\n");
+        return 1; 
+    } 
+    volume = (4.0 * PI * radius * radius * radius) / 3.0;
+
+    printf("\n--- Result ---\n");
+    printf("Radius entered: %.2f units\n", radius);
+    printf("The volume of the sphere is: %.4f cubic units\n", volume);
+    printf("----------------\n");
+
+    return 0; 
 }
+
