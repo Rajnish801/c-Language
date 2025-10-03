9a3ea0b76f801db1bba948b686a451a54994dc23
Rajnish Kumar Singh <rajnishkrsingh801@gmail.com>
2025-10-03 00:16:45 -0500

    cal volume1.c

--- cal volume1.c
+++ cal volume1.c
@@ -12,16 +12,16 @@ int main() {
    
     if (scanf("%lf", &radius) != 1) {
         printf("Error: Invalid input. Please enter a number.\n");
-        return 1; 
+        
     }
 
     if (radius < 0) {
         printf("Error: Radius cannot be negative. Please enter a positive value.\n");
-        return 1; 
+        
     } 
     volume = (4.0 * PI * radius * radius * radius) / 3.0;
 
-    printf("\n--- Result ---\n");
+    
     printf("Radius entered: %.2f units\n", radius);
     printf("The volume of the sphere is: %.4f cubic units\n", volume);
 
