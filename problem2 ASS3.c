1cc97a6438ea774b6b46b196527b868be591ceac
Rajnish Kumar Singh <rajnishkrsingh801@gmail.com>
2025-10-03 01:58:40 -0500

    problem2 ASS3.c

--- problem2 ASS3.c
+++ problem2 ASS3.c
@@ -2,12 +2,9 @@
 
 int main() {
     int m, n;
-
-    // Prompt the user to enter an integer
     printf("Enter an integer value for m: ");
     scanf("%d", &m);
 
-    // Determine the value of n based on m
     if (m > 0) {
         n = 1;
     } else if (m == 0) {
@@ -16,7 +13,6 @@ int main() {
         n = -1;
     }
 
-    // Display the result
     printf("The value of n is: %d\n", n);
 
     return 0;
